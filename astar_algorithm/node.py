9abf57a1e8775e2ce8c7931ddef5a1a99f2142ff
jane133 <jane1231001@gmail.com>
2018-12-05 11:24:32 +0900

    Feat: git test git test content

--- astar_algorithm/node.py
+++ astar_algorithm/node.py
@@ -4,33 +4,6 @@ class Node:
         self.H = H
         self.parent = parent
         self.location = location
-
-    @property
-    def data(self):
-        print("F({}) = G({}) + H({})".format(self.F, self.G, self.H))
-
-    @data.setter
-    def data(self, G, H):
-        self.G = G
-        self.H = H
-        self.F = self.G + self.H
-
-    @property
-    def parent(self):
-        print("parrent Node is {}.".format(parent))
-
-    @parent.setter
-    def parent(self, parent):
-        self.parent = parent
-
-    @property
-    def location(self):
-        print("current location is {}.".format(location))
-
-    @location.setter
-    def location(self, location):
-        self.location = location
-    print('a')
-
+        
 
 #test for git
