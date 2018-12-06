c297a88c23d07777b2a7155f04e659e839a7e758
EmjayAhn <emjay.data@gmail.com>
2018-12-06 16:38:42 +0900

    Feat: Edit node structure

--- astar_algorithm/node.py
+++ astar_algorithm/node.py
@@ -1,10 +1,8 @@
 class Node:
     def __init__(self, G=None, H=None, parent=None, location=None):
+
         self.G = G
         self.H = H
+        self.F = self.G + self.H
         self.parent = parent
         self.location = location
-
-        #끊임없는 test
-
-#test for git
