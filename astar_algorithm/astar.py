c9028f2715303dbc938d9bc3ca8f68da3b28d7c9
EmjayAhn <emjay.data@gmail.com>
2018-12-06 20:10:11 +0900

    Feat: Debug errors -

--- astar_algorithm/astar.py
+++ astar_algorithm/astar.py
@@ -84,8 +84,8 @@ class Astar:
 
 
 
-    def calculate(NODE):
-        ```
+    def calculate(self, NODE):
+        '''
         parameter : NODE - Node() 자료구조
         return : 없음 - NODE 객체 안에 data (F, G, H)를 변경
 
@@ -94,18 +94,17 @@ class Astar:
         NODE.G 의 경우, 부모로부터 온 방향을 찾아 DIR dictionary 에서 10 혹은 14 값을 받아, 부모의 G 값에서 더합니다.
         NODE.H 의 경우, self.GOAL 까지의 거리 입니다. 이 거리는 Manhattan Path Scoring 방법을 사용합니다.
         NODE.F 의 경우, NODE.G와 NODE.H를 더합니다.
-        ```
+        '''
         #Testing 을 클래스 밖에서 간단하게 해서 잘 돌아갔으나, class 안에서 다시한번 해볼 필요가 있습니다.
 
         #NODE 가 부모로부터 어느 방향으로부터 왔는가
         #이것을 알아야 10을 더할지 14를 더할지 DIR 에서 선택할 수 있다.
-        direction = NODE.location[0] - NODE.parent[0], NODE.location[1] - NODE.location[1]
-
+        direction = NODE.location[0] - NODE.parent[0], NODE.location[1] - NODE.parent[1]
         #G 값계산
         NODE.G = self.maze_dict[NODE.parent].G + DIR[direction]
         #H 값계산
-        NODE.H = abs(self.GOAL.location[0] - NODE.location[0]) + \
-        abs(self.GOAL.location[1] - NODE.location[1])
+        NODE.H = (abs(self.GOAL.location[0] - NODE.location[0]) + \
+        abs(self.GOAL.location[1] - NODE.location[1])) * 10
         #F 값계산
         NODE.F = NODE.G + NODE.H
 
@@ -139,7 +138,7 @@ class Astar:
         # 그 노드를 포함해서 상/하/좌/우 에 있는 노드까지 tmp_barrier로 취급하여
         # 5개의 노드를 tmp_barrier에 추가
         for i in around_nodes:
-            if i in barrier_list:
+            if i in self.barrier_list:
                 i_east = tuple(np.array(i) + np.array([1, 0]))
                 i_west = tuple(np.array(i) + np.array([-1, 0]))
                 i_north = tuple(np.array(i) + np.array([0, 1]))
@@ -149,12 +148,12 @@ class Astar:
         # 다시 around_nodes를 검색하여
         # opened_list, tmp_barrier 모두에 없는 노드만 opened_list에 추가
         for i in around_nodes:
-            if (i not in tmp_barrier) and (i not in opened_list):
+            if (i not in tmp_barrier) and (i not in self.opened_list):
                 self.opened_list.append(i)
 
 
     def sorting_open(self, opened_list):
-        sort_open = [(i, maze_dict[i].F) for i in opened_list]
+        sort_open = [(i, maze_dict[i].F) for i in self.opened_list]
         opened_list = [j[0] for j in sorted(sort_open, key=lambda i: i[1])]
         return opened_list
 
