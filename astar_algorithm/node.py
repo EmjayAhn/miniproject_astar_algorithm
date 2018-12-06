d9ef7054a2eb832862d8621e40be24943fbc8d99
EmjayAhn <emjay.data@gmail.com>
2018-12-06 17:19:06 +0900

    Feat: edit Node Class default value

--- astar_algorithm/node.py
+++ astar_algorithm/node.py
@@ -1,5 +1,5 @@
 class Node:
-    def __init__(self, G=None, H=None, parent=None, location=None):
+    def __init__(self, G=0, H=0, parent=None, location=None):
 
         self.G = G
         self.H = H
