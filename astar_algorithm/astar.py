f9240c1dad144bdac3c76dd0a0d15bee9651f224
EmjayAhn <emjay.data@gmail.com>
2018-12-10 23:47:47 +0900

    Feat: Finally We DID it

--- astar_algorithm/astar.py
+++ astar_algorithm/astar.py
@@ -1,7 +1,7 @@
-import numpy as np
-
+# import numpy as np
 
 class Astar:
+
 
     DIR = {
         (-1, 1): 14,
@@ -24,21 +24,19 @@ class Astar:
         self.START = Node(location=start)
         self.GOAL = Node(location=goal)
         self.path = list()
+        self.CUR = Node()
 
 
     def encode_map(self):
         # 외곽벽 추가
         EXIT_ROW = len(self.maze_map)    # 행의 갯수
         EXIT_COL = len(self.maze_map[0]) # 열의 갯수
-
         for row in self.maze_map:        # 첫번째열 마지막열 1로 채운다
             row.insert(0, 1)
             row.append(1)
-
         added_row = [1 for _ in range(EXIT_COL+2)]  # 첫번째행, 마지막행 1로 채운다
         self.maze_map.insert(0, added_row)
         self.maze_map.append(added_row)
-
         # x,y 좌표 직관적으로 바꾸기
         # maze_dict location node 할당
         for y in range(len(maze_map)-1, -1, -1):
@@ -48,19 +46,17 @@ class Astar:
                 self.maze_dict[(x_loc,y_loc)] = Node(location =(x_loc,y_loc))
                 if value == 1:
                     self.barrier_list.append((x_loc, y_loc))
+        self.init_barrier = self.barrier_list
 
 
     def start_goal(self):
         start_location = self.START.location
         goal_location = self.GOAL.location
-
         # START G,H value
         G = 0
-        H = abs((goal_location[0]-start_location[0]) + (goal_location[1]-start_location[1]))
-
+        H = abs((goal_location[0]-start_location[0]) + (goal_location[1]-start_location[1])) * 10
         # START NODE H value
         self.START.H = H
-
         # START NODE and GOAL NODE maze dict에 할당
         self.maze_dict[start_location] = self.START
         self.maze_dict[goal_location] = self.GOAL
@@ -68,35 +64,42 @@ class Astar:
 
     def search_around(self, NODE):
         # 일단 search하는 중심 노드는 closed_list에 추가
-        self.closed_list.append(NODE.location)
+        if NODE.location not in self.closed_list:
+            self.closed_list.append(NODE.location)
         # 주변 노드 좌표 리스트 생성
         around_nodes = list(map(tuple, np.array(list(Astar.DIR.keys())) + NODE.location))
+
+        for i in around_nodes:
+            if not self.maze_dict[i].parent:
+                self.maze_dict[i].parent = NODE.location
+#             print("i", i, "cur", self.CUR.location, "parent", self.maze_dict[i].parent)
+            #self.calculate(self.maze_dict[i])
 
         return around_nodes
 
 
-    def astar_algorithm(self, CUR, cur_around):
+    def astar_algorithm(self, cur_around):
         for i in cur_around:
             if i in self.opened_list:
-                new_G = CUR.G + self.DIR[(self.maze_dict[i].location[0] - CUR.location[0], self.maze_dict[i].location[1] - CUR.location[1])]
+                new_G = self.CUR.G + self.DIR[(self.maze_dict[i].location[0]\
+                        - self.CUR.location[0], self.maze_dict[i].location[1] - self.CUR.location[1])]
                 old_G = self.maze_dict[i].G
                 if new_G >= old_G:
                     continue
                 else:
-                    self.maze_dict[i].parent = CUR.location
+                    self.maze_dict[i].parent = self.CUR.location
+                    self.calculate(self.maze_dict[i])
             elif (i in self.closed_list) | (i in self.barrier_list):
                 continue
             else:
                 self.calculate(self.maze_dict[i])
                 self.push_open(i)
-                self.maze_dict[i].parent = CUR.location
 
 
     def calculate(self, NODE):
         '''
         parameter : NODE - Node() 자료구조
         return : 없음 - NODE 객체 안에 data (F, G, H)를 변경
-
         NODE 가 들어왔을 때, 그 G, H, F 를 계산하여, 다시 해당 NODE.G, NODE.H, NODE.F에
         할당해 줍니다.
         NODE.G 의 경우, 부모로부터 온 방향을 찾아 DIR dictionary 에서 10 혹은 14 값을 받아, 부모의 G 값에서 더합니다.
@@ -107,7 +110,6 @@ class Astar:
         #NODE 가 부모로부터 어느 방향으로부터 왔는가
         #이것을 알아야 10을 더할지 14를 더할지 DIR 에서 선택할 수 있다.
         direction = NODE.location[0] - NODE.parent[0], NODE.location[1] - NODE.parent[1]
-
         #G 값계산
         NODE.G = self.maze_dict[NODE.parent].G + self.DIR[direction]
         #H 값계산
@@ -117,58 +119,38 @@ class Astar:
         NODE.F = NODE.G + NODE.H
 
 
-    def push_open(self, location):
-        # search_around 함수와 동일하게 around_nodes 리스트 생성
-        around_nodes = list(map(tuple, np.array(list(Astar.DIR.keys())) + location))
+    def push_open(self, my_location):
+        self.opened_list.append(my_location)
 
-        location_NE = tuple(np.array(location) + np.array([1, 1]))
-        location_NW = tuple(np.array(location) + np.array([-1, 1]))
-        location_SE = tuple(np.array(location) + np.array([1, -1]))
-        location_SW = tuple(np.array(location) + np.array([-1, -1]))
-
-        diagonal_path = [location_NE, location_NW, location_SW, location_SE]
-        diagonal_location_direction = [[(-1,0),(0,-1)],[(1,0),(0,-1)],[(0,1),(1,0)],[(0,1),(-1,0)]]
-        print(diagonal_path)
-
-        for location in around_nodes:
-            if (location not in self.barrier_list) and (location not in self.opened_list) and (location not in diagonal_path):
-                self.opened_list.append(location)
-
-        for location in zip(diagonal_path, diagonal_location_direction):
-            if tuple(np.array(location[0]) + np.array(location[1][0])) not in self.barrier_list:
-                if tuple(np.array(location[0]) + np.array(location[1][1])) not in self.barrier_list:
-                    self.opened_list.append(location[0])
+    def sorting_open(self):
+        self.opened_list = sorted(self.opened_list, key=lambda x: self.maze_dict[x].F, reverse=True)
 
 
-
-    def sorting_open(self, opened_list, reverse=False):
-
-        sort_open = [(i, self.maze_dict[i].F) for i in opened_list]
-        opened_list = [j[0] for j in sorted(sort_open, key=lambda i: i[1], reverse=reverse)]
-        return opened_list
-
-
-    # , maze_map, start_location, goal_location
     def maze_solver(self):
         self.encode_map()  # self.maze_map 생성
         self.start_goal()  # start, goal 노드 생성 완료
-        CUR = self.START   # CUR를 START 노드로 설정 완료
-
-        cur_around = self.search_around(CUR) # start의 search_around
-
+        self.CUR = self.START   # CUR를 START 노드로 설정 완료
+        cur_around = self.search_around(self.CUR) # start의 search_around
         while self.GOAL.location not in cur_around:
-            self.astar_algorithm(CUR, cur_around)
-            sorting_open(reverse=True)
-            CUR = self.maze_dict[self.opened_list.pop()]
-            cur_around = self.search_around(CUR)
+            self.astar_algorithm(cur_around)
+            self.sorting_open()
 
-        self.GOAL.G = CUR.G + self.DIR[self.GOAL.location[0] - CUR.location[0],\
-                                 self.GOAL.loaction[1] - CUR.loaction[1]]
-        self.GOAL.parent = CUR.location
+            if len(self.opened_list) != 0:
+                self.CUR = self.maze_dict[self.opened_list.pop()]
+                cur_around = self.search_around(self.CUR)
 
-        while CUR.location != self.START.location:
-            self.path.append(CUR.location)
-            CUR = self.maze_dict[CUR.parent]
+
+        self.GOAL.G = self.CUR.G + self.DIR[self.GOAL.location[0] - self.CUR.location[0],\
+                                            self.GOAL.location[1] - self.CUR.location[1]]
+        self.GOAL.parent = self.CUR.location
+
+        self.path.append(self.GOAL.location)
+
+        while self.CUR.location != self.START.location:
+            self.path.append(self.maze_dict[self.path[-1]].parent)
+            self.CUR = self.maze_dict[self.CUR.parent]
+
         self.path.reverse()
 
+        self.path.insert(0, self.START.location)
         return self.path, self.GOAL.G
